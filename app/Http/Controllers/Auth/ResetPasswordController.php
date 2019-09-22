b2df198e800751de746c993e6071d0d963b67fbb
Endrju0 <andrzej640@gmail.com>
2019-09-22 19:16:58 +0200

    redirect fix

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/map';
 
     /**
      * Create a new controller instance.
